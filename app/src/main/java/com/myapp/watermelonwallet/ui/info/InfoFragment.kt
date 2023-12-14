61e23ac2bbfa4058d79cd95f77553c64b34ad9d6
Ariel Antequiera <arielantequiera@gmail.com>
2023-12-14 19:57:40 -0300

    add info layout

--- app/src/main/java/com/myapp/watermelonwallet/ui/info/InfoFragment.kt
+++ app/src/main/java/com/myapp/watermelonwallet/ui/info/InfoFragment.kt
@@ -28,10 +28,7 @@ class InfoFragment : Fragment() {
         _binding = FragmentInfoBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        infoViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
