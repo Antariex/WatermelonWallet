c970ac6f7df4ae4873a947026460a35d9e3e7973
Ariel Antequiera <arielantequiera@gmail.com>
2023-12-31 15:48:16 -0300

    fix recyclerview to show added wallets

--- app/src/main/java/com/myapp/watermelonwallet/ui/wallets/WalletsFragment.kt
+++ app/src/main/java/com/myapp/watermelonwallet/ui/wallets/WalletsFragment.kt
@@ -1,12 +1,20 @@
 package com.myapp.watermelonwallet.ui.wallets
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.myapp.watermelonwallet.R
 import com.myapp.watermelonwallet.databinding.FragmentWalletsBinding
 import com.myapp.watermelonwallet.ui.WalletActivity
@@ -18,6 +26,7 @@ class WalletsFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var walletAdapter: WalletAdapter
+    private var wallets: MutableList<Wallet> = mutableListOf() // Initialize an empty list
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,12 +39,7 @@ class WalletsFragment : Fragment() {
         recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        // Ejemplo de billeteras (deberías obtenerlas de SharedPreferences)
-        val wallets = listOf(
-            Wallet(1, "Billetera 1", 100.0f, "USD"),
-            Wallet(2, "Billetera 2", 50.0f, "EUR")
-            // Agrega tus billeteras aquí
-        )
+        wallets = getWalletsFromSharedPreferences() // Load wallets from SharedPreferences
 
         walletAdapter = WalletAdapter(wallets)
         recyclerView.adapter = walletAdapter
@@ -44,6 +48,17 @@ class WalletsFragment : Fragment() {
         textView.text = "Mis Billeteras"
 
         return root
+    }
+
+    private fun getWalletsFromSharedPreferences(): MutableList<Wallet> {
+        val prefs = requireContext().getSharedPreferences("MyPrefsFile", Context.MODE_PRIVATE)
+        val walletsString = prefs.getString("wallets", null)
+
+        return if (walletsString != null) {
+            Gson().fromJson(walletsString, object : TypeToken<MutableList<Wallet>>() {}.type)
+        } else {
+            mutableListOf()
+        }
     }
 
     override fun onDestroyView() {
